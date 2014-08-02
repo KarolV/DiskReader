cb66b654430a7d417e8f10c8e9f65ed97d5ac7f3
KarolV <karol.vecerek@chello.sk>
2014-08-02 16:07:31 +0200

    Initial commit: Disk reader app created

---
+++ DiskReader/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DiskReader
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 15);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(91, 12);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(41, 21);
+			this.comboBox1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(249, 10);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// treeView1
+			// 
+			this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.treeView1.Location = new System.Drawing.Point(12, 39);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(307, 198);
+			this.treeView1.TabIndex = 3;
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(331, 22);
+			this.statusStrip1.TabIndex = 4;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(316, 17);
+			this.toolStripStatusLabel1.Spring = true;
+			this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(138, 14);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(80, 17);
+			this.checkBox1.TabIndex = 5;
+			this.checkBox1.Text = "checkBox1";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(331, 262);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.treeView1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.CheckBox checkBox1;
+	}
+}
+
---
+++ DiskReader/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DiskReader.Properties;
+
+namespace DiskReader
+{
+	public partial class Form1 : Form
+	{
+		private DriveInfo driveInfo;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			InitializeControls();
+
+			this.driveInfo = DriveInfo.GetDrives().FirstOrDefault(d => d.DriveType == DriveType.CDRom);
+
+			this.comboBox1.SelectedIndexChanged +=
+				(s, e) =>
+				{
+					this.driveInfo =
+						DriveInfo.GetDrives()
+						         .FirstOrDefault(d => d.Name == ((DriveInfo) this.comboBox1.SelectedItem).Name);
+
+					this.ShowDriveInfoStatus();
+				};
+
+			this.button1.Click +=
+				(s, e) =>
+				{
+					this.treeView1.Nodes.Clear();
+					this.ShowDriveInfoStatus();
+
+					if (this.driveInfo.IsReady)
+						BuildTree(this.driveInfo.RootDirectory, this.treeView1.Nodes);
+
+					this.treeView1.ExpandAll();
+				};
+
+			this.comboBox1.SelectedItem = this.driveInfo;
+
+			this.ShowDriveInfoStatus();
+		}
+
+		private void InitializeControls()
+		{
+			this.Text = Resources.Form_Title_DiskReader;
+			this.label1.Text = Resources.Label_Title_FocusOnDisk;
+			this.button1.Text = Resources.Button_Title_Reload;
+			this.checkBox1.Text = Resources.CheckBox_Title_DirectoriesOnly;
+			this.checkBox1.Checked = true;
+			this.comboBox1.DataSource =
+				DriveInfo.GetDrives()
+				         .Where(d => d.DriveType == DriveType.CDRom)
+				         .ToList();
+		}
+
+		private void ShowDriveInfoStatus()
+		{
+			this.toolStripStatusLabel1.Text = this.driveInfo == null
+				? string.Empty
+				: string.Format("{0} [{1}]",
+				                this.driveInfo.Name,
+				                this.driveInfo.IsReady
+					                ? this.driveInfo.VolumeLabel
+					                : Resources.DiskSatatus_InsertDisk);
+		}
+
+		private void BuildTree(DirectoryInfo directoryInfo, TreeNodeCollection treeNodeCollection)
+		{
+			var currentNode = treeNodeCollection.Add(directoryInfo.Name);
+
+			if(!this.checkBox1.Checked)
+				foreach (var fileInfo in directoryInfo.GetFiles())
+					currentNode.Nodes.Add(fileInfo.FullName, fileInfo.Name);
+
+			foreach (var subdir in directoryInfo.GetDirectories())
+				BuildTree(subdir, currentNode.Nodes);
+		}
+	}
+}
